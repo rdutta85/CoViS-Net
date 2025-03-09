064c9ae24ddd5ac7eceb908562e36b8f1031b396
Rishav Dutta <rishavdutta@umass.edu>
2025-03-09 19:13:38 +0000

    removed conda setup commands

--- download_data.sh
+++ download_data.sh
@@ -8,7 +8,4 @@ fi
 export MATTERPORT_PW=602fc3c4dfce15d6c89013e1a6e2b799
 export MATTERPORT_ID=fed6fccb2f05b202
 
-conda init
-conda activate convisnet
-
 python -m habitat_sim.utils.datasets_download --username $MATTERPORT_ID --password $MATTERPORT_PW --uids $data_uid
